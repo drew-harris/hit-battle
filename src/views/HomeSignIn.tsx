0624fddb0576ea3d2367638e999e7fccab6d8130
Drew Harris <dsharris10@gmail.com>
2022-09-25 01:03:32 -0500

    Add sponsored message

--- src/views/HomeSignIn.tsx
+++ src/views/HomeSignIn.tsx
@@ -5,10 +5,16 @@ import Button from "../components/input/Button";
 const HomeSignIn: NextPage = () => {
   return (
     <div className="grid h-[80vh] place-items-center overflow-hidden ">
-      <div className="mx-auto flex flex-col items-center gap-4 text-center">
+      <div className="mx-auto items-center  rounded-lg bg-tan-100 p-8 text-center shadow-sm">
         <h1 className=" text-2xl font-bold text-tan-500">
           STEM PLAYER COMMUNITY HIT BATTLE
         </h1>
+        <h3 className="mb-8 mt-0  text-tan-400">
+          Sponsored by&nbsp;
+          <span className="underline">
+            <a href="https://www.stemify2.net">Stemify</a>
+          </span>
+        </h3>
         <Button
           onClick={() => signIn("discord")}
           label="Sign In With Discord"
