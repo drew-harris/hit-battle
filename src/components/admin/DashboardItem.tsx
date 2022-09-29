bc855c4fdb78132592bbf078410a3f946bd766a7
Drew Harris <dsharris10@gmail.com>
2022-09-29 00:20:44 -0500

    Add loaders to admin dashboard

--- src/components/admin/DashboardItem.tsx
+++ src/components/admin/DashboardItem.tsx
@@ -1,4 +1,8 @@
 import Link from "next/link";
+import { PulseLoader } from "react-spinners";
+
+import resolveConfig from "tailwindcss/resolveConfig";
+import tailwindConfig from "../../../tailwind.config.cjs";
 
 interface DashboardItemProps {
   title?: string;
@@ -12,13 +16,17 @@ export default function DashboardItem(props: DashboardItemProps) {
   return (
     <div className="rounded-lg bg-tan-100  p-4 text-center shadow-md">
       {props.title && <div className="mb-2">{props.title}</div>}
-      {props.value !== null && (
+      {props.value || props.value === 0 ? (
         <div
           className={`${
             props.size === "small" ? "text-xl" : "text-3xl"
           } font-bold`}
         >
           {props.value}
+        </div>
+      ) : (
+        <div className="text-tan- mt-4">
+          <PulseLoader size={6} color="rgb(84, 71, 56)" />
         </div>
       )}
       {props.children && <div>{props.children}</div>}
--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -5,61 +5,57 @@ export default function AdminHome() {
   const { data } = trpc.useQuery(["admin.dashboard-info"]);
   return (
     <div className="flex flex-wrap items-start justify-around gap-4 md:justify-start">
-      {data && (
-        <>
-          <DashboardItem
-            title="Total Users"
-            value={data.userCount}
-            href="/admin/users"
-            linkLabel="View Users"
-          />
-          <DashboardItem
-            title="Total Songs"
-            value={data.songsCount}
-            href="/admin/songs"
-            linkLabel="All Songs"
-          />
-          <DashboardItem
-            title="Artists"
-            value={data.artistCount}
-            href="/admin/addartist"
-            linkLabel="Add Artist"
-          />
-          <DashboardItem
-            title="Current Matches"
-            value={data.currentMatchCount}
-            href="/admin/matches"
-            linkLabel="View Matches"
-          />
-          <DashboardItem
-            title="Total Matches"
-            value={data.totalMatchCount}
-            href="/admin/create-matches"
-            linkLabel="Create Matches"
-          />
-          <DashboardItem
-            linkLabel="Create Custom Match"
-            href="/admin/custom-match"
-            title="Custom Matches"
-            value={data.customMatchCount}
-          />
-          <DashboardItem
-            title="Songs With Previews"
-            value={data.previewUrlCount}
-          />
-          <DashboardItem title="Total Votes" value={data.voteCount} />
-          <DashboardItem
-            title="Most Popular Song"
-            size="small"
-            value={
-              "" +
-              data.mostPopularSong?.title +
-              " - " +
-              data.mostPopularSong?.artist
-            }
-          />
-        </>
-      )}
+      <DashboardItem
+        title="Total Users"
+        value={data?.userCount}
+        href="/admin/users"
+        linkLabel="View Users"
+      />
+      <DashboardItem
+        title="Total Songs"
+        value={data?.songsCount}
+        href="/admin/songs"
+        linkLabel="All Songs"
+      />
+      <DashboardItem
+        title="Artists"
+        value={data?.artistCount}
+        href="/admin/addartist"
+        linkLabel="Add Artist"
+      />
+      <DashboardItem
+        title="Current Matches"
+        value={data?.currentMatchCount}
+        href="/admin/matches"
+        linkLabel="View Matches"
+      />
+      <DashboardItem
+        title="Total Matches"
+        value={data?.totalMatchCount}
+        href="/admin/create-matches"
+        linkLabel="Create Matches"
+      />
+      <DashboardItem
+        linkLabel="Create Custom Match"
+        href="/admin/custom-match"
+        title="Custom Matches"
+        value={data?.customMatchCount}
+      />
+      <DashboardItem
+        title="Songs With Previews"
+        value={data?.previewUrlCount}
+      />
+      <DashboardItem title="Total Votes" value={data?.voteCount} />
+      <DashboardItem
+        title="Most Popular Song"
+        size="small"
+        value={
+          "" +
+          data?.mostPopularSong?.title +
+          " - " +
+          data?.mostPopularSong?.artist
+        }
+      />
     </div>
   );
 }
